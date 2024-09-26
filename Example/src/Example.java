c6479105990cf0c8bec20b161edda86f61746eca
defrey-mo <bwire.fredrick.19@gmail.com>
2024-09-26 21:31:54 +0300

    Add files via upload

---
+++ Example/src/Example.java
@@ -0,0 +1,15 @@
+public class Example {
+    public static void main(String[] args) throws Exception {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Access the element at the specified row and column
+        int element = matrix[1][2];
+
+        // Print the element
+        System.out.println(element);
+    }
+}
